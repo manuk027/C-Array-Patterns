e194710f77d06e001420df42c46b85bb00d6d9b3
Manu Krishna <krishnamanu27112004@gmail.com>
2025-05-24 21:39:47 +0530

    Create Pattern2.c

---
+++ Day 6/Pattern2.c
@@ -0,0 +1,26 @@
+/*
+
+* * * * *
+* *     *
+*   *   *
+*     * *
+* * * * *
+
+ */
+
+#include <stdio.h>
+int main(){
+	int i, j, n=5;
+	for(i=1; i<=n; i++){
+		for(j=1; j<=n; j++){
+			if(i==1 || j==1 || i==j || i==5 || j==5){
+				printf("* ");
+			} else {
+				printf("  ");
+			}
+
+		}
+		printf("\n");
+	}
+	return 0;
+}
