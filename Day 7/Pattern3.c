3ff11a8a6b9a83966b1a3060f6a408f9bb29e44f
Manu Krishna <krishnamanu27112004@gmail.com>
2025-05-24 21:10:17 +0530

    Create Pattern3.c

---
+++ Day 7/Pattern3.c
@@ -0,0 +1,26 @@
+/*
+ 	 	 	 	     A
+ 	 	 	 	    B C
+ 	 	 	 	   D E F
+ 	 	 	 	  G H I J
+ 	 	 	 	 K L M N O
+ */
+
+#include <stdio.h>
+int main(){
+
+	int i, j, n=4;
+	char alpha='A';
+
+	for(i=1; i<=n; i++){
+
+		for(j=1; j<=n+1; j++){
+			printf("%c ", alpha);
+			alpha++;
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
