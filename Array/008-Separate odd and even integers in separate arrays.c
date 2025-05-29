2db3de5350f45ebde4e84a9f38449cc0317e8a9e
Manu Krishna <krishnamanu27112004@gmail.com>
2025-05-29 14:29:25 +0530

    Create 008-Separate odd and even integers in separate arrays.c

---
+++ Array/008-Separate odd and even integers in separate arrays.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int i, arr[100], odd[100], even[100], size, e=0, o=0;
+    printf("Enter the size : ");
+    scanf("%d", &size);
+    printf("Enter %d elements : ", size);
+    for(i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i]%2==0){
+            even[e]=arr[i];
+            e++;
+        } else {
+            odd[o]=arr[i];
+            o++;
+        }
+    }
+    printf("Array of even numbers : ");
+    for(i=0; i<e; i++){
+        printf("%d\t", even[i]);
+    }
+    printf("Array of odd numbers : ");
+    for(i=0; i<o; i++){
+        printf("%d\t", odd[i]);
+    }
+    
+}
