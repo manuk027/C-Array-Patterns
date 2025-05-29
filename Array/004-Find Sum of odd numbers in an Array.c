4c80793da309c47e06938d3494f4b50dbb255e3c
Manu Krishna <krishnamanu27112004@gmail.com>
2025-05-29 14:05:38 +0530

    Create 004-Find Sum of odd numbers in an Array.c

---
+++ Array/004-Find Sum of odd numbers in an Array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i, sum=0, arr[100], size;
+    printf("Enter the size of the array : ");
+    scanf("%d", &size);
+    printf("Enter numbers : ");
+    for(i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i]%2==1){
+            sum+=arr[i];
+        }
+    }
+    printf("The sum of even numbers is %d", sum);
+    return 0;
+}
