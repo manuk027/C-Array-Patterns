67b5b4ab6faed72274fee8bc333d9957b587d137
Manu Krishna <krishnamanu27112004@gmail.com>
2025-05-29 14:18:45 +0530

    Update 007-Copy the element of one array to another.c

--- Array/007-Copy the element of one array to another.c
+++ Array/007-Copy the element of one array to another.c
@@ -6,12 +6,10 @@ int main(){
     printf("Enter %d elements : ", size);
     for(i=0; i<size; i++){
         scanf("%d", &arr[i]);
-    }
-    for(i=0; i<size; i++){
         arr2[i]=arr[i];
     }
     printf("Copied array is : ");
     for(i=0; i<size; i++){
-        printf("\t%d", arr2[i]);
+        printf("%d\t", arr2[i]);
     }
 }
